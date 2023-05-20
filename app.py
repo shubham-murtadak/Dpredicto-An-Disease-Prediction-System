298d54dd9936cc0ed2e72bc64b2f053835516028
shubham murtadak <shubhammurtadak011@gmail.com>
2023-05-20 19:34:45 +0530

    chatbot added as a seperate page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask,render_template,request,jsonify
+from chat import get_response
 app = Flask(__name__)
 
 @app.route('/')
@@ -25,6 +25,19 @@ def heart():
 @app.route('/parkinsons')
 def parkinsons():
     return render_template('parkinsons.html')
+
+@app.get("/chat")
+
+def index_get():
+    return render_template("cbase.html")
+
+@app.post("/predict")
+def predict():
+    text=request.get_json().get("message")
+    response=get_response(text)
+    message={"answer":response}
+    return jsonify(message)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
